8ec8e39888f908bc1f7e062eb7cdf619a15fa570
Zehra Tok <zehratok1@outlook.com>
2022-11-30 15:53:53 +0300

    add constants folder

---
+++ FoodyApp/src/constants/index.js
@@ -0,0 +1,7 @@
+import constants from "./constants";
+import dummyData from "./dummyData"
+import icons from "./icons";
+import images from "./images";
+import theme, { COLORS, SIZES, FONTS } from "./theme";
+
+export { constants, dummyData, icons, images, theme, COLORS, SIZES, FONTS };
