ba5c1a21efaca717649c6c2059ced97d2d02a8df
Zehra Tok <zehratok1@outlook.com>
2022-12-02 16:50:16 +0300

    add Redux
    - npm install redux react-redux redux-thunk

--- FoodyApp/src/App.js
+++ FoodyApp/src/App.js
@@ -2,31 +2,38 @@ import React from "react";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 import { AddCard, Checkout, DeliveryStatus, FoodDetail, ForgotPassword, Map, MyCart, MyCard, OnBoarding, Otp, SignIn, SignUp, Success } from "./screens";
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import rootReducer from './stores/rootReducer';
 import CustomDrawer from "./navigation/CustomDrawer";
 
 const Stack = createNativeStackNavigator();
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 const App = () => {
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName={'OnBoarding'} >
-      <Stack.Screen name="OnBoarding" component={OnBoarding}/*Home ile yeri değişti */ />
-        <Stack.Screen name="Home" component={MainLayout} />
-        <Stack.Screen name="SignIn" component={SignIn} />
-        <Stack.Screen name="SignUp" component={SignUp} />
-        <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
-        <Stack.Screen name="Otp" component={Otp} />
-        <Stack.Screen name="FoodDetail" component={FoodDetail} />
-        <Stack.Screen name="Checkout" component={Checkout} />
-        <Stack.Screen name="MyCart" component={MyCart} />
-        <Stack.Screen name="Success" component={Success} />
-        <Stack.Screen name="AddCard" component={AddCard} />
-        <Stack.Screen name="MyCard" component={MyCard} />
-        <Stack.Screen name="DeliveryStatus" component={DeliveryStatus} />
-        <Stack.Screen name="Map" component={Map} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Home" component={CustomDrawer} />
+          <Stack.Screen name="OnBoarding" component={OnBoarding} />
+          <Stack.Screen name="SignIn" component={SignIn} />
+          <Stack.Screen name="SignUp" component={SignUp} />
+          <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
+          <Stack.Screen name="Otp" component={Otp} />
+          <Stack.Screen name="FoodDetail" component={FoodDetail} />
+          <Stack.Screen name="Checkout" component={Checkout} />
+          <Stack.Screen name="MyCart" component={MyCart} />
+          <Stack.Screen name="Success" component={Success} />
+          <Stack.Screen name="AddCard" component={AddCard} />
+          <Stack.Screen name="MyCard" component={MyCard} />
+          <Stack.Screen name="DeliveryStatus" component={DeliveryStatus} />
+          <Stack.Screen name="Map" component={Map} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   )
 }
 
