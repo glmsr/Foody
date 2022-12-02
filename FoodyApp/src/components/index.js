65674e784fcf265cdc876bc17a08a9545963776d
Zehra Tok <zehratok1@outlook.com>
2022-12-03 02:07:17 +0300

    add various component

--- FoodyApp/src/components/index.js
+++ FoodyApp/src/components/index.js
@@ -1,3 +1,8 @@
-export {Header} from './Header';
+import CustomSwitch from './CustomSwitch';
+import FormInput from './FormInput';
 import Header from './Header';
+import SocialButton from './SocialButton';
 import TextButton from './TextButton';
+import TextIconButton from './TextIconButton';
+
+export { CustomSwitch, FormInput, Header, SocialButton, TextButton, TextIconButton };
