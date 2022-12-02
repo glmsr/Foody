0e66438aa474b772853eeb0c0667b8b2c2189672
Zehra Tok <zehratok1@outlook.com>
2022-12-03 02:08:35 +0300

    add ForgotPassword screen

--- FoodyApp/src/styles/Auth.style.js
+++ FoodyApp/src/styles/Auth.style.js
@@ -33,6 +33,92 @@ const styles = StyleSheet.create({
     childContainer: {
         marginHorizontal: SIZES.margin
     },
+    signInContainer: {
+        flex: 1,
+        marginTop: SIZES.padding,
+    },
+    formInputContainer: {
+        marginTop: SIZES.radius,
+    },
+    appendComponentMailContainer: {
+        justifyContent: 'center',
+    },
+    appendComponentMailIcon: {
+        width: 20,
+        height: 20,
+    },
+    appendComponentPassContainer: {
+        width: 40,
+        alignItems: 'flex-end',
+        justifyContent: 'center',
+    },
+    appendComponentPassIcon: {
+        width: 20,
+        height: 20,
+        tintColor: COLORS.gray
+    },
+    saveMeContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginTop: SIZES.radius,
+    },
+    forgotPassButtonContainer: {
+        backgroundColor: null,
+    },
+    forgotPassLabel: {
+        color: COLORS.gray,
+        ...FONTS.body4,
+    },
+    signInButtonContainer: {
+        height: 55,
+        alignItems: 'center',
+        marginTop: SIZES.padding,
+        borderRadius: SIZES.radius,
+    },
+    signInButtonLabel: {
+        ...FONTS.h3,
+    },
+    redirectContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        marginTop: SIZES.radius
+    },
+    redirectText: {
+        color: COLORS.darkGray,
+        ...FONTS.body3
+    },
+    redirectButtonContainer: {
+        backgroundColor: null,
+    },
+    redirectButtonLabel: {
+        color: COLORS.primary,
+        ...FONTS.h3,
+    },
+    socialButtonContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        marginTop: SIZES.padding * 2,
+    },
+    fbButtonContainer: {
+        height: 50,
+        alignItems: 'center',
+        borderRadius: SIZES.radius,
+        backgroundColor: COLORS.blue,
+        marginHorizontal: SIZES.base,
+    },
+    fbIconStyle: {
+        tintColor: COLORS.white,
+    },
+    googleButtonContainer: {
+        height: 50,
+        alignItems: 'center',
+        borderRadius: SIZES.radius,
+        backgroundColor: COLORS.lightGray2,
+        marginHorizontal: SIZES.base
+    },
+    googleIconStyle: {
+        tintColor: null
+    },
 });
 
 export default styles;
