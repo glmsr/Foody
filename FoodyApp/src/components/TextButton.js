e9449809a156491786aba0ac7d9d4d8e6a9cc69c
Zehra Tok <49477192+zehratok@users.noreply.github.com>
2022-12-06 00:40:44 +0300

    fix some bugs

--- FoodyApp/src/components/TextButton.js
+++ FoodyApp/src/components/TextButton.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import { TouchableOpacity, Text, ActivityIndicator } from "react-native";
+import { TouchableOpacity, Text, ActivityIndicator, StyleSheet } from "react-native";
 import { FONTS, COLORS } from '../constants';
 
 const TextButton = ({ buttonContainerStyle, label, labelStyle, loading, onPress, disabled }) => {
     return (
-        <TouchableOpacity style={{
-            alignItems: 'center',
-            justifyContent: 'center',
-            backgroundColor: COLORS.primary,
-            ...buttonContainerStyle
-        }} onPress={onPress} disabled={disabled}>
-
-            <Text style={{
-                color: 'white',
-                ...FONTS.h3,
-                ...labelStyle
-            }} >
-              { loading ? <ActivityIndicator color={COLORS.white} /> : label }
-            </Text>
-
+        <TouchableOpacity style={[styles.container, {buttonContainerStyle }]} onPress={onPress} disabled={disabled}>
+            <Text style={[styles.label, {labelStyle}]} > { loading ? <ActivityIndicator color={COLORS.white} /> : label } </Text>
         </TouchableOpacity>
     );
 }
 export default TextButton;
+
+const styles = StyleSheet.create({
+    container: {
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: COLORS.primary,
+    },
+    label: {
+      color: 'white',
+      ...FONTS.h3,
+    },
+});
