e2b7f96a6d3ed8ddef6a075dc743c77a021ef513
mehmetaligundogdu <gundogdu.102@outlook.com>
2022-12-09 01:35:57 +0300

    Create Firebase for IOS

--- FoodyApp/src/navigation/AppStack.js
+++ FoodyApp/src/navigation/AppStack.js
@@ -8,12 +8,12 @@ const Stack = createNativeStackNavigator();
 const AppStack = () => {
   return (
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="FoodDetail" component={FoodDetail} />
         <Stack.Screen name="Home" component={CustomDrawer} />
+        <Stack.Screen name="AddCard" component={AddCard} />
+        <Stack.Screen name="FoodDetail" component={FoodDetail} />
         <Stack.Screen name="Checkout" component={Checkout} />
         <Stack.Screen name="MyCart" component={MyCart} />
         <Stack.Screen name="Success" component={Success} options={{ gestureEnabled: false}} />
-        <Stack.Screen name="AddCard" component={AddCard} />
         <Stack.Screen name="MyCard" component={MyCard} />
         <Stack.Screen name="DeliveryStatus" component={DeliveryStatus} options={{ gestureEnabled: false}}/>
         <Stack.Screen name="Map" component={Map} />
