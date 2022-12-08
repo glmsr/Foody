4904b5928769bcbe215b18fc5c6163feea8d8baa
glmsr <gulumseren35@gmail.com>
2022-12-08 23:14:01 +0300

    add checkout and success screen features

--- FoodyApp/src/navigation/AppStack.js
+++ FoodyApp/src/navigation/AppStack.js
@@ -8,14 +8,14 @@ const Stack = createNativeStackNavigator();
 const AppStack = () => {
   return (
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Home" component={CustomDrawer} />
         <Stack.Screen name="FoodDetail" component={FoodDetail} />
+        <Stack.Screen name="Home" component={CustomDrawer} />
         <Stack.Screen name="Checkout" component={Checkout} />
         <Stack.Screen name="MyCart" component={MyCart} />
-        <Stack.Screen name="Success" component={Success} />
+        <Stack.Screen name="Success" component={Success} options={{ gestureEnabled: false}} />
         <Stack.Screen name="AddCard" component={AddCard} />
         <Stack.Screen name="MyCard" component={MyCard} />
-        <Stack.Screen name="DeliveryStatus" component={DeliveryStatus} />
+        <Stack.Screen name="DeliveryStatus" component={DeliveryStatus} options={{ gestureEnabled: false}}/>
         <Stack.Screen name="Map" component={Map} />
       </Stack.Navigator>
   )
